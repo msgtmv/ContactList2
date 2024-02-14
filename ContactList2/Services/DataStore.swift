79777d56f44b6d764489f74914ff917626375891
Muslim Gapparov <muslimgapparov@gmail.com>
2024-02-14 16:00:02 +0300

    Initial Commit

---
+++ ContactList2/Services/DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  ContactList2
+//
+//  Created by Муслим Гаппаров on 14.02.2024.
+//
+
+import Foundation
---
+++ ContactList2/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList2
+//
+//  Created by Муслим Гаппаров on 07.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
